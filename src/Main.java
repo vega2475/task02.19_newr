bab7dde099f98e2aa1ff4281865149ea9f5d7e59
vega <devid367552@gmail.com>
2022-09-27 22:33:54 +0300

    s

--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,20 @@ public class Main {
         return Math.sqrt(2) * tetragonSide;
     }
 
-    public static void minValueCircles(double tetragonSide, double diameter, double radius) {
+    public static int minValueCircles(double tetragonSide, double diameter, double radius, int count) {
 
         if (radius < diameter / 4) {
-            System.out.println("4 круга не достаточно");
+            count = -1;
         } else if (radius >= diameter / 2) {
-            System.out.println("Достаточно минимум 1 круг");
-        } else if (Math.pow(radius, 2) >= (Math.pow(tetragonSide, 2) / 4) + (Math.pow(tetragonSide, 2) / 16) && radius >= diameter / 4) {
-            System.out.println("Достаточно минимум 2 круга");
-        } else if (radius >= diameter / 4 && Math.pow(radius, 2) >= (Math.pow(tetragonSide, 2) / 4) + (Math.pow(tetragonSide, 2) / 36)) {
-            System.out.println("Достаточно минимум 3 круга");
-        } else if (radius >= diameter / 4) {
-            System.out.println("4 круга достаточно");
+            count = 1;
+        } else if (Math.pow(radius, 2) >= (Math.pow(tetragonSide, 2) / 4) + (Math.pow(tetragonSide, 2) / 16)) {
+            count = 2;
+        } else if (Math.pow(radius, 2) >= (Math.pow(tetragonSide, 2) / 4) + (Math.pow(tetragonSide, 2) / 36)) {
+            count = 3;
+        } else  {
+            count = 4;
         }
-
+        return count;
     }
     public static void readValues() {
         var scanner = new Scanner(System.in);
@@ -30,9 +30,13 @@ public class Main {
 
         var diameter = diameterTetragon(tetragonSide);
 
+        int count = 0;
+
         System.out.println("Радиус 1 круга = " + radius);
 
-        minValueCircles(tetragonSide, diameter, radius);
+        int counter = minValueCircles(tetragonSide, diameter, radius, count);
+
+        System.out.println(counter);
 
     }
 
