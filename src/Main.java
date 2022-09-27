2a90b726810f732a820abf63e44deb264fc4cbf7
vega <devid367552@gmail.com>
2022-09-27 21:27:57 +0300

    s

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,22 @@ public class Main {
         return Math.sqrt(2) * tetragonSide;
     }
 
+    public static void minValueCircles(double tetragonSide, double diameter, double radius) {
 
-    public static void result() {
+        if (radius < diameter / 4) {
+            System.out.println("4 круга не достаточно");
+        } else if (radius >= diameter / 2) {
+            System.out.println("Достаточно минимум 1 круг");
+        } else if (Math.pow(radius, 2) >= (Math.pow(tetragonSide, 2) / 4) + (Math.pow(tetragonSide, 2) / 16) && radius >= diameter / 4) {
+            System.out.println("Достаточно минимум 2 круга");
+        } else if (radius >= diameter / 4 && Math.pow(radius, 2) >= (Math.pow(tetragonSide, 2) / 4) + (Math.pow(tetragonSide, 2) / 36)) {
+            System.out.println("Достаточно минимум 3 круга");
+        } else if (radius >= diameter / 4) {
+            System.out.println("4 круга достаточно");
+        }
+
+    }
+    public static void readValues() {
         var scanner = new Scanner(System.in);
 
         System.out.print("Pls write side of tetragon: ");
@@ -18,36 +32,16 @@ public class Main {
 
         System.out.println("Радиус 1 круга = " + radius);
 
-
-
-
-        if (radius  < diameter / 4 ) {
-            System.out.println("4 круга не достаточно");
-        }
-
-        else if(radius >= diameter / 2 ){
-            System.out.println("Достаточно минимум 1 круг");
-        }
-
-        else if(Math.pow(radius, 2) >= (Math.pow(tetragonSide, 2) / 4) + (Math.pow(tetragonSide, 2) / 16) && radius >= diameter / 4){
-            System.out.println("Достаточно минимум 2 круга");
-        }
-
-        else if (radius >= diameter / 4 && Math.pow(radius, 2) >= (Math.pow(tetragonSide, 2) / 4) + (Math.pow(tetragonSide, 2) / 36)) {
-            System.out.println("Достаточно минимум 3 круга");
-        }
-
-        else if (radius >= diameter / 4) {
-            System.out.println("4 круга достаточно");
-        }
-
-
-
-
+        minValueCircles(tetragonSide, diameter, radius);
 
     }
 
+
+
+
+
+
     public static void main(String[] args){
-        result();
+        readValues();
     }
 }
